91dc82f4c39e71e4b0d1afd26b2913d2ab628388
徐旭 <1605611836@qq.com>
2018-07-03 16:39:19 +0800

    更新数据库

--- db/DBConfig.js
+++ db/DBConfig.js
@@ -6,7 +6,7 @@ module.exports = {
 		host: '223.2.197.241',
 		user: 'root',
 		password: 'nsi-dev',
-		database: 'fastdfs',
+		database: 'ftp',
 		port: 3306,
 		//强制日期类型，需要配合mysql数据库使用，另外，mysql模块默认编码是utf-8
 		dateStrings: 'DATETIME'
